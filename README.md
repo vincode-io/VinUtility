299d2f57ce1bc124a173996dd13fe1ad3224c66d
Maurice Parker <mo@vincode.io>
2024-09-27 14:52:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# VinUtility
+
+Utility code for Zavala. Eventually this package may become a separate project with useful code that could be
+used by multiple Vincode projects.
---
+++ Sources/VinUtility/UICollectionView+.swift
@@ -0,0 +1,26 @@
+//
+//  UICollectionView+.swift
+//  Zavala
+//
+//  Created by Maurice Parker on 1/12/21.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+public extension UICollectionView {
+
+    func deselectAll(animated: Bool = true) {
+		indexPathsForSelectedItems?.forEach { indexPath in
+			deselectItem(at: indexPath, animated: animated)
+		}
+	}
+
+	func isVisible(indexPath: IndexPath) -> Bool {
+		guard let cell = cellForItem(at: indexPath) else { return false }
+		return bounds.contains(convert(cell.frame, to: self))
+	}
+}
+
+#endif
